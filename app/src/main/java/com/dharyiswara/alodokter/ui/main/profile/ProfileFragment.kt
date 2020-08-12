a858e3153aded3439e36aa36952e021a9fd89d68
Dary Hilmy Iswara <dary.iswara@redkendi.com>
2020-08-12 17:57:33 +0700

    add profile fragment

--- app/src/main/java/com/dharyiswara/alodokter/ui/main/profile/ProfileFragment.kt
+++ app/src/main/java/com/dharyiswara/alodokter/ui/main/profile/ProfileFragment.kt
@@ -2,8 +2,16 @@ package com.dharyiswara.alodokter.ui.main.profile
 
 import com.dharyiswara.alodokter.R
 import com.dharyiswara.alodokter.base.BaseFragment
+import com.dharyiswara.alodokter.helper.loadFromUrl
+import com.dharyiswara.alodokter.preferences.UserSession
+import com.dharyiswara.alodokter.ui.login.LoginActivity
+import kotlinx.android.synthetic.main.fragment_profile.*
+import org.jetbrains.anko.support.v4.startActivity
+import org.koin.android.ext.android.inject
 
 class ProfileFragment : BaseFragment() {
+
+    private val userSession by inject<UserSession>()
 
     companion object {
 
@@ -11,6 +19,24 @@ class ProfileFragment : BaseFragment() {
 
     }
 
-    override fun getLayoutResId(): Int = R.layout.fragment_home
+    override fun getLayoutResId(): Int = R.layout.fragment_profile
+
+    override fun initView() {
+        super.initView()
+
+        ivAvatar.loadFromUrl("https://avatarfiles.alphacoders.com/139/thumb-139523.jpg")
+        tvName.text = getString(R.string.string_username)
+        tvGender.text = getString(R.string.string_gender_male)
+        tvPhoneNumber.text = getString(R.string.string_phone_number)
+    }
+
+    override fun initEvent() {
+        super.initEvent()
+        tvLogout.setOnClickListener {
+            userSession.logout()
+            startActivity<LoginActivity>()
+            requireActivity().finish()
+        }
+    }
 
 }
